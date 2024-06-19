2ba63d006f457b04844f92b095e861a8f2298cdc
bt9988 <reallyrealdesigns@gmail.com>
2024-06-19 16:47:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,29 +7,4 @@ document.addEventListener('DOMContentLoaded', function() {
     menuToggle.addEventListener('click', function() {
         navList.classList.toggle('active');
     });
-
-    // Homepage: Load teams.json and dynamically generate team buttons
-    fetch('team/teams.json')
-        .then(response => response.json())
-        .then(data => {
-            const teamButtonsContainer = document.querySelector('.team-buttons');
-
-            // Create buttons for each team on homepage
-            Object.keys(data).forEach(team => {
-                const teamName = data[team].teamName;
-
-                const teamButton = document.createElement('button');
-                teamButton.classList.add('team-button');
-                teamButton.textContent = teamName;
-                teamButton.setAttribute('data-team', team); // Keep track of the team name
-                teamButtonsContainer.appendChild(teamButton);
-
-                // Add click event listener to each team button
-                teamButton.addEventListener('click', function() {
-                    const teamName = teamButton.getAttribute('data-team');
-                    window.location.href = `team/${teamName}.html`; // Navigate to team page
-                });
-            });
-        })
-        .catch(error => console.error('Error fetching teams.json:', error));
 });
