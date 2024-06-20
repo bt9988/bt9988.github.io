bf7bc60a76d3a7e182eefd59938bb7dce9837aba
bt9988 <reallyrealdesigns@gmail.com>
2024-06-20 15:22:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", async function() {
     try {
         const response = await fetch('teams.json');
-        const data = await response.json();
+        const teams = await response.json();
         
-        populateTeams(data);
-        setupTeamPage(data);
+        populateTeams(teams);
+        setupTeamPage(teams);
     } catch (error) {
         console.error('Error fetching team data:', error);
     }
@@ -29,16 +29,12 @@ document.addEventListener("DOMContentLoaded", async function() {
         const team = teams.find(t => t.name === teamName);
         if (!team) return;
 
+        document.title = `${team.name} | Team Page | 🚨 NHL Goal Songs 🚨`;
         document.getElementById('team-name').textContent = team.name;
         document.getElementById('team-name-placeholder').textContent = team.name;
         document.getElementById('song-name').textContent = team.currentGoalSong.name;
         document.getElementById('artist-name').textContent = team.currentGoalSong.artist;
         document.getElementById('youtube-iframe').src = `https://www.youtube.com/embed/${team.currentGoalSong.youtubeID}`;
-
-        const dynamicParagraph = document.getElementById('dynamicParagraph');
-        if (dynamicParagraph) {
-            dynamicParagraph.textContent = `The current goal song for the NHL's ${team.name} is ${team.currentGoalSong.name} by ${team.currentGoalSong.artist}.`;
-        }
     }
 
     // Function to navigate to team page with the team name as query parameter
