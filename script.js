dd51d136d56326639e666a52febefb1c9bcbfb5c
bt9988 <reallyrealdesigns@gmail.com>
2024-07-13 13:24:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -134,3 +134,17 @@ document.addEventListener("DOMContentLoaded", async function() {
 
     window.navigateToTeam = navigateToTeam;
 });
+
+// Navigation menu functionality
+function toggleMenu() {
+    var menu = document.getElementById("sideMenu");
+    if (menu.style.width === "250px") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "250px";
+    }
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+}
